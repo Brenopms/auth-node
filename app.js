8e173d60daf8bc9040ef614e55fcd2fb6f296a5b
Breno Silva <brenosilva1@gmail.com>
2017-12-19 18:21:53 -0200

    add post routes

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/svcc');
 
-app.use(bodyParser, urlencoded({ extended:True}))
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended:true}))
 
 //User model
 const Schema = mongoose.Schema;
+let objectId = Schema.objectId;
 
+let User = mongoose.model('User', new Schema ({
+    // id: objectId,
+    firstName: String,
+    lastName: String,
+    email: {type: String, unique: true},
+    password: String,
+},{usePushEach: true}));
 
 
 app.set('view engine', 'jade');
@@ -26,11 +34,45 @@ app.get('/register', (req, res) => {
 
 //POST Route
 app.post('/register', (req,res) => {
-    res.json(req.body);
+    let user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+
+    });
+
+    user.save(function(err){
+        if(err){
+            let error = 'Something bad happened! Please try again';
+
+            if(error.code === 11000){
+                error = 'That email is alredy taken, please try another';
+            }
+
+            res.render('register.jade', {error: error});
+        } else {
+            res.redirect('/dashboard');
+        }
+    });
 });
 
 app.get('/login', (req, res) => {
     res.render('login.jade');
+});
+
+app.post('/login', (req,res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user){
+            res.render('login.jade', {error: 'Incorrect email/password'});
+        } else {
+            if(req.body.password === user.password){
+                res.redirect('/dashboard');
+            } else {
+                res.render('login.jade', {error: 'incorrect email/password'});
+            }
+        }
+    });
 });
 
 app.get('/dashboard', (req, res) => {
