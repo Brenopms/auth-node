1ca495ce34b6e5e3594d59ee3cbcd1de16224aa3
Breno Silva <brenosilva1@gmail.com>
2017-12-19 19:36:19 -0200

    add csrf protection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('client-sessions');
+const bcrypt = require('bcryptjs');
 
 mongoose.connect('mongodb://localhost/svcc');
 
@@ -33,6 +34,36 @@ let User = mongoose.model('User', new Schema ({
 
 app.set('view engine', 'jade');
 
+//Smart user middleware
+
+app.use((req,res, next) => {
+    if(req.session && req.session.user) {
+        models.User.findOne({email: req.session.user.email}, (err, user) =>{
+            //if a user was found, make the user avaliable
+            if(user) {
+                req.user = user;
+                delete req.user.password; //don't make the password hash avaliable
+                req.session.user = user; //update the session info
+                res.locals.user = user; //make the user avaliable to templates
+            }
+            next();
+        });
+    } else {
+        next(); //if no session is avaliable, do nothing
+    }
+});
+
+function requireLogin(req, res, next) {
+    //if this user isn't logged in, redirect them to the login page
+    if(!req.user){
+        res.redirect('/login');
+        //if the user is logged in, let them pass
+    } else {
+        next();
+    }
+};
+
+
 app.get('/', (req, res) => {
     res.render('index.jade');
 });
@@ -43,11 +74,14 @@ app.get('/register', (req, res) => {
 
 //POST Route
 app.post('/register', (req,res) => {
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(req.body.password, salt);
+
     let user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: hash,
     });
 
     user.save(function(err){
@@ -74,7 +108,7 @@ app.post('/login', (req,res) => {
         if(!user){
             res.render('login.jade', {error: 'Incorrect email/password'});
         } else {
-            if(req.body.password === user.password){
+            if(bcrypt.compareSync(req.body.password, user.password)) {
                 req.session.user = user;
                 res.redirect('/dashboard');
             } else {
@@ -84,7 +118,7 @@ app.post('/login', (req,res) => {
     });
 });
 
-app.get('/dashboard', (req, res) => {
+app.get('/dashboard', requireLogin, (req, res) => {
     if(req.session && req.session.user){
         User.findOne({email: req.session.user.email}, (err, user) => {
             if(!user){
