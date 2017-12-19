fa780546050bb44673ecd88707591b4952923dd9
Breno Silva <brenosilva1@gmail.com>
2017-12-19 17:12:12 -0200

    add mongoose and schema

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+'use strict';
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/svcc');
+
+app.use(bodyParser, urlencoded({ extended:True}))
 
 const app = express();
+
+//User model
+const Schema = mongoose.Schema;
+
+
 
 app.set('view engine', 'jade');
 
@@ -10,6 +22,11 @@ app.get('/', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register.jade');
+});
+
+//POST Route
+app.post('/register', (req,res) => {
+    res.json(req.body);
 });
 
 app.get('/login', (req, res) => {
